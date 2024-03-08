bab68e03fdf379d0e52c257bf5693c337e94992e
Drailan John Terrible <drailan@thebluerock.com.au>
2024-03-08 21:36:19 +0800

    chore:initial scaffolding

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,33 @@
-import React from 'react';
+/* eslint-disable react/button-has-type */
 
-export default function Login() {
-  return <div>Login</div>;
+'use client';
+
+import React from 'react';
+import axios from 'axios';
+
+const loginAction = async (): Promise<void> => {
+  axios.defaults.xsrfHeaderName = 'X-CSRFTOKEN';
+  axios.defaults.xsrfCookieName = 'csrftoken';
+
+  await axios
+    .post(`${process.env.NEXT_PUBLIC_APP_API}/api/v1/login`, {
+      email: 'admin@email.com',
+      password: 'password',
+    })
+    .then((res) => {
+      console.log(res);
+    })
+    .catch((error) => {
+      console.error('Login failed:', error);
+    });
+};
+
+function Login() {
+  return (
+    <div>
+      <button onClick={loginAction}>Login</button>
+    </div>
+  );
 }
+
+export default Login;
